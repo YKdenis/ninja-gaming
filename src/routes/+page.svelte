a72c5864de803f1776be8afd437400fa53c33b2a
Denis <denis.inghelbrecht@outlook.be>
2023-01-04 16:50:53 +0100

    lesson 3 data binding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let title = 'Ninja Gaming Guides';
+
+	const updateTitle = () => {
+		title = 'Something completely different!';
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa voluptatum illo cum
+		assumenda ipsa quisquam sunt suscipit maiores, placeat quo, quas aut aperiam laboriosam
+		voluptatibus nam eum quis repellat!
+	</p>
+	<button on:click={updateTitle}>Change title</button>
+	<input type="text" bind:value={title} />
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		margin: 20px auto;
+	}
+</style>
