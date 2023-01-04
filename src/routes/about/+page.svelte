4e4b3cb39af1dd2b4d6544922f83882405811743
Denis <denis.inghelbrecht@outlook.be>
2023-01-04 17:08:03 +0100

    lesson 4 routing

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus deleniti, dolore, recusandae
+		ex labore consequuntur aspernatur fugit molestias quibusdam aut quidem, eos commodi nemo
+		voluptatum non? Numquam recusandae commodi laborum!
+	</p>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		margin: 20px auto;
+	}
+</style>
