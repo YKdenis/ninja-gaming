12edd3e7955b0a75844498f35e2661c970a091b0
Denis <denis.inghelbrecht@outlook.be>
2023-01-28 16:56:00 +0100

    reset layouts and and laoding data & SSR

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Title from '$lib/title.svelte';
+	import '../../styles/global.css';
+</script>
+
+<header>
+	<Title title="Ninja Gaming" />
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/guides">Guides</a>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		max-width: 1080px;
+		margin: auto;
+	}
+	nav {
+		display: flex;
+		justify-content: space-around;
+		min-width: 180px;
+	}
+	main {
+		max-width: 1080px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+</style>
---
+++ src/routes/(guides)/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Title from '$lib/title.svelte';
+	import '../../styles/global.css';
+</script>
+
+<header>
+	<Title title="Ninja Gaming" />
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/guides">Guides</a>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		flex-direction: row-reverse;
+		justify-content: space-between;
+		align-items: center;
+		max-width: 1080px;
+		margin: auto;
+	}
+	nav {
+		display: flex;
+		justify-content: space-around;
+		min-width: 180px;
+	}
+	main {
+		max-width: 1080px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+</style>
